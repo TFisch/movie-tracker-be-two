35e6c9fc4d3f9b2e5226d49e288fcd998ae9c09e
Tim Fischer <emailtimfischer@gmail.com>
2018-10-15 12:42:04 -0600

    Initial commit

---
+++ knexfile.js
@@ -0,0 +1,12 @@
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/publications',
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  }
+};
+
+
