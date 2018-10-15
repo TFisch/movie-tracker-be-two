79b574f37a6acea8698f07d17132d78afc1bf004
Tim Fischer <emailtimfischer@gmail.com>
2018-10-15 12:48:02 -0600

    Set default port

---
+++ server.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const express = require('express');
+const cors = require('express-cors');
+var bodyParser = require('body-parser');
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+const bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static('public'));
